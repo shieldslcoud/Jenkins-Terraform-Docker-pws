f8a6981946b25383a2b3a62b76b4a87089c3699e
pdasilva11 <50679298+pdasilva11@users.noreply.github.com>
2023-11-02 17:17:49 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -13,9 +13,19 @@ variable "key_name" {
   default = "tests"
   description = "Desired name of AWS key pair"
 }
-
 # Defining Region
 variable "aws_region" {}
+
+# Defining PasswordSafe
+variable "api_key" {
+  default = "c7c3cad5b2c6b6ddfa869bd8ca3a3869d4364100876fbfa43b42d308d109078d90cdda7b89e6b5a99f81175a368448667cb012af7f96a32a97150085ce6bc055"
+}
+variable "url" {
+default = "https://ea8fe8.ps.beyondtrustcloud.com/BeyondTrust/api/public/v3"
+}
+variable "api_account_name" {
+  default = "darce"
+}
 
 # Defining CIDR Block for VPC
 variable "vpc_cidr" {
